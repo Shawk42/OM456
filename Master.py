0ddffd0110c4800a9eba8952c7fbc89e5b35b063
Deryk <derahner@outlook.com>
2019-02-04 18:03:33 -0800

    Problem 3 Unverified, have a small deviation that I am unsure about. Needs further clarification

--- Master.py
+++ Master.py
@@ -1,20 +1,18 @@
-
-
 """INPUTS"""
 
-TGT = 2            #Nominal Value
-mu = "null"             #Average Value
-UTL = "ans"         #Upper tolerance limit
-LTL = "ans"         #Lower tolerance limit
-SD = 0.001          #Standard deviation
-Tol_sigma = "null"  #Tolerance range but in sigma
-K = 6          #Sigma Level
-CP = 1.3        #CP index
-UCL = "ans"
+TGT = 2                    #Nominal Value
+mu = "null"                 #Average Value
+UTL = 2.0039             #Upper tolerance limit
+LTL = 1.9961             #Lower tolerance limit
+SD =  0.001  #Standard deviation
+Tol_sigma = "null"          #Tolerance range but in sigma
+K = 6                      #Sigma Level
+CP = 1.3                   #CP index
+UCL = 2.003
 LCL = "ans"
 
 """COMMON CALCS"""  #May have to comment out illegal operations
-#Tol_range = UTL-LTL  #Found by dimensions
+Tol_range = UTL-LTL  #Found by dimensions
 Cntrl_range = K*SD
 Tolerance_range = CP*Cntrl_range   #Range that dimenions need to be in
 
@@ -53,6 +51,20 @@ elif CP == "ans":
 
 else:
     print("No answer solved for")
+
+
+"""VERIFICATION"""
+left = K*SD
+right = Tol_range/2
+dev = abs(right-left)
+#dev = 0
+if dev <= 0.0001:
+    print("Verifed")
+else:
+    print("Not verified")
+    print("Deviation",dev)
+    print("Left",left)
+    print("Right",right)
 
 print(""*50)
 print("Numerical Summary")
