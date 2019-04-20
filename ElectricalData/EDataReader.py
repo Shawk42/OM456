8f722ad832311711f17b73859bd2597b2b2062d3
Deryk <derahner@outlook.com>
2019-04-20 16:56:18 -0700

    Electric Data Outages Found

--- ElectricalData/EDataReader.py
+++ ElectricalData/EDataReader.py
@@ -1,43 +1,37 @@
 import numpy as np
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
-from tempfile import TemporaryFile
-outfile = TemporaryFile()
 
 """IMPORTATION"""
-print("STATUS - Starting import")
 east = genfromtxt('EastFeed.csv',delimiter= ',')
-print("STATUS - East Feed Imported")
 west = genfromtxt('WestFeed.csv',delimiter=',')
-print("STATUS - West Feed Imported")
-
-print("STAUS - Data Importation Complete")
 
 """DATA EXTRACTION"""
-print("STATUS - Data Extraction Started")
-
 west_pwr = west[:,6]
 east_pwr = east[:,6]
-#time = east[:,2]
 
-
-print("STATUS - Data Extraction Complete")
-
-"""DATA FILTERING"""
-print("STATUS - Data Cleaning Started")
+"""DATA CLEANING"""
 west_pwr = np.delete(west_pwr,0)
 east_pwr = np.delete(east_pwr,0)
 
-print("STATUS - Data Cleaning Complete")
-
-
-
-
-
 length = len(west_pwr)
-time = np.linspace(0,length,length)
 
-plt.subplots_adjust(hspace=0.4)
+"""OUTAGE DETECTION"""
+
+west_pwrout = np.zeros(length)
+
+np.copyto(west_pwrout,west_pwr)
+np.putmask(west_pwrout,west_pwrout>1,-1)
+west_pwrout = 1+west_pwrout
+
+east_pwrout = np.zeros(length)
+np.copyto(east_pwrout,east_pwr)
+np.putmask(east_pwrout,east_pwrout>1,-1)
+east_pwrout = 1+east_pwrout
+
+
+
+plt.subplots_adjust(hspace=0.4,wspace=0.4)
 
 plt.subplot(2,2,1)
 plt.plot(west_pwr)
@@ -49,6 +43,14 @@ plt.plot(east_pwr)
 plt.grid()
 plt.title("East Feed")
 
+plt.subplot(2,2,2)
+plt.plot(west_pwrout)
+plt.grid()
+plt.title("West Feed Outage")
 
+plt.subplot(2,2,4)
+plt.plot(east_pwrout)
+plt.grid()
+plt.title("East Feed Outage")
 
 plt.show()
